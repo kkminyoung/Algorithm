c8976b86d8fd1db6eaae2a2364b7b8e579e518fa
kmy0433 <kmy0433@naver.com>
2020-07-22 23:28:59 +0900

    algorithm

---
+++ baekjoon/sol_2749.cpp
@@ -0,0 +1,25 @@
+// 피사노 주기를 이용한 피보나치수 문제
+// 2749
+
+#include<iostream>
+#include <stdio.h>
+
+long long a[1500050];
+int I = 1000000; // 피보나치수를 나눌 수
+
+void fibo() {
+	a[0] = 0;
+	a[1] = 1;
+	//I로 나눈 피보나치 수열의 반복 주기는 1500000 이다.
+	for (int i = 0; i < 1500000; i++) {
+		a[i + 2] = (a[i + 1] + a[i]) % I;
+	}
+}
+
+int main() {
+	long long n;
+	std::cin >> n;
+	fibo();
+	std::cout << a[n % 1500000] << "\n";
+
+}
