f0021fc34dbc078c549fc8789a46074b6cf738c9
Kim Minyoung <61506233+kkminyoung@users.noreply.github.com>
2020-12-23 23:05:45 +0900

    동적계획법

---
+++ programmers/동적계획법/등굣길.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+    
+    int map[100][100]={0,}; // 지도
+    int ans[100][100]; // 정답
+    
+    // 물웅덩이는 1로
+    for(int i=0;i<puddles.size();i++){
+        map[puddles[i][1]][puddles[i][0]]= 1;
+    }
+    
+    ans[1][0] = 1; // 초기값
+    
+    // 최단경로 누적
+    for(int i=1 ; i<=n ; i++){
+        for(int j=1; j<=m; j++){
+            if(map[i][j]==1){
+                ans[i][j]=0;
+            }
+            else{
+                ans[i][j] = (ans[i-1][j] + ans[i][j-1])% 1000000007;
+            }
+        }
+    }
+    answer = ans[n][m];
+    
+    return answer;
+}
