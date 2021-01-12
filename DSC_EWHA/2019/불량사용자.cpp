c9108b3bf99497bedce6115095671e36be572700
Kim Minyoung <61506233+kkminyoung@users.noreply.github.com>
2021-01-12 17:22:44 +0900

    Update 불량사용자.cpp

--- DSC_EWHA/2019/불량사용자.cpp
+++ DSC_EWHA/2019/불량사용자.cpp
@@ -29,30 +29,30 @@ void dfs(vector<string> u_id, vector<string> b_id, int b_ind){
         bool tmp = true;
         
         // 사이즈가 다르거나 이미 사용한 경우 제외
-		if (b_id[b_ind].size() != u_id[i].size() || check[i]) {
-			continue;
-		}
+	if (b_id[b_ind].size() != u_id[i].size() || check[i]) {
+		continue;
+	}
         
         // banned_id가 될 수 있는지
-		for (int j = 0; j < u_id[i].size(); j++) {
-			if (b_id[b_ind][j] == '*') {
-				continue;
-			}
-
-            // 글자가 다른 경우 false로
-			if (u_id[i][j] != b_id[b_ind][j]) {
-				tmp = false;
-				break;
-			}
+	for (int j = 0; j < u_id[i].size(); j++) {
+		if (b_id[b_ind][j] == '*') {
+			continue;
 		}
 
-        // 가능한 경우라면 
-		if (tmp) {
-			check[i] = true;
-			dfs(u_id, b_id, b_ind + 1);
-			check[i] = false;
+                // 글자가 다른 경우 false로
+		if (u_id[i][j] != b_id[b_ind][j]) {
+			tmp = false;
+			break;
 		}
 	}
+
+        // 가능한 경우라면 
+	if (tmp) {
+		check[i] = true;
+		dfs(u_id, b_id, b_ind + 1);
+		check[i] = false;
+	}
+    }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
